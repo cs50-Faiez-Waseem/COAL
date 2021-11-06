39af663b07381736ee3240680b7d03c44046c650
Faiez Waseem <75004269+cs50-Faiez-Waseem@users.noreply.github.com>
2021-11-06 09:08:32 +0500

    11/6/21

---
+++ suboftwoinput.asm
@@ -0,0 +1,30 @@
+;Taking two input and sub them
+; when we get value from user we are recieving ascii code
+.model small
+.stack 100h
+.data
+.code
+main proc
+ 
+;taking First One From User
+mov ah,1
+int 21h  
+
+mov dl,al
+
+;taking Second One From User
+mov ah,1
+int 21h
+
+sub dl,al 
+
+add dl,48
+
+mov ah,02
+int 21h
+
+mov ah,4ch
+int 21h    
+
+main endp
+end main
